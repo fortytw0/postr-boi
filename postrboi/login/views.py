ff5786e73d26e122f5ab64f37bf5bb379303dda3
Dananjay Srinivas <dsrinivas@dsrinivas.local>
2020-04-24 11:49:16 +0530

    Terminal based functionality. Shelfed temporarily.

--- postrboi/login/views.py
+++ postrboi/login/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate , login
 from login.forms import UserForm
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -32,10 +36,52 @@ def register(request) :
         else : 
 
             print(user_form.errors)
+            return render(request , 'login/base.html' , context={'user_form':user_form , 'registered':registered , 'signup':True})
 
     else : 
 
         user_form = UserForm()
 
-    return render(request , 'login/base.html' , context={'user_form':user_form , 'registered':registered})
+    return render(request , 'login/base.html' , context={'user_form':user_form , 'registered':registered , 'signup':True})
+
+
+def signin(request) : 
+
+    login_successful = False
+    
+    if request.method == 'POST' : 
+
+        print('authenticating...')
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username , password=password)
+
+        if user : 
+
+            if user.is_active :
+
+                login(request , user)
+                return redirect(reverse('blog:index'))
+
+            else :
+
+                return HttpResponse('<h1>USER IS NOT ACTIVE!</h1>')
+
+        else : 
+            
+            return HttpResponse('<h1>login credentials invalid!</h1>')
+
+        
+    else: 
+
+        return render(request , 'login/base.html' , context={'login':True})
+
+
+                
+
+
+
+
 
